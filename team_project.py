d30f9d89a34751bb230410ecd4c984cb5d0b3845
jabim <sabin20030720@gmail.com>
2022-05-26 11:22:20 +0900

    Crawling 01

--- team_project.py
+++ team_project.py
@@ -17,7 +17,7 @@ options.add_argument('--disable-dev-shm-usage')
 options.add_argument('disable-gpu')
 driver = webdriver.Chrome('./chromedriver', options=options)
 df_titles = pd.DataFrame()
-for i in range(0, 2):
+for i in range(2, 4):
     titles = []
     for j in range(1,pages[i]+1):
         url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}#&date=%2000:00:00&page={}'.format(i, j)
